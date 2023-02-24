e4268670b98e5c757f21f15e76d99cd0c44da812
rizorno <114782234+rizorno@users.noreply.github.com>
2023-02-24 21:45:51 +0100

    Updates

--- src111/route/PublicRoute.js
+++
@@ -1,9 +0,0 @@
-import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
-import authSelectors from 'redux/auth/authSelectors';
-
-const PublicRoute = ({ children }) => {
-  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
-  return isLoggedIn ? <Navigate to="/contacts" /> : children;
-};
-export default PublicRoute;
