5358c80e0c0d9dc702c9833b64a6278067de51b9
rizorno <114782234+rizorno@users.noreply.github.com>
2023-09-16 13:15:12 +0100

    updates

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,18 @@
+import { logo } from './icons';
 import css from './footer.module.scss';
 
 const Footer = () => (
   <footer className={css.footer}>
-    <p>PhoneBook &copy; 2023 Created by </p>
-    <a
+    <p>PhoneBook &copy; 2023 Created by</p>
+    {/* <a
       href="https://github.com/rizorno"
       target="_blank"
          rel="noreferrer noopener"
          className={css['footer-link']}
     >
       Mykhailo PASHKO
-    </a>
+    </a> */}
+    <div>{logo(100, 30)}</div>
   </footer>
 );
 
